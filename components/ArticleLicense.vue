2aa5751ee825a1c4f90e768419a8ac80e8b98be2
Jannchie <jannchie@gmail.com>
2021-05-01 02:18:41 +0900

    feat: shorten license on xs screens

--- components/ArticleLicense.vue
+++ components/ArticleLicense.vue
@@ -4,11 +4,23 @@
       <i class="fas fa-balance-scale"></i>
     </div>
     <div>
-      <b> Creative Commons Attribution Share Alike 4.0 International </b>
+      <b v-if="!xs">
+        Creative Commons Attribution Share Alike 4.0 International
+      </b>
+      <b v-else> CC BY-SA 4.0</b>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  computed: {
+    xs() {
+      if (process.client) {
+        return window.innerWidth < 600
+      }
+      return false
+    },
+  },
+}
 </script>
